535d7f18396e6de08a32c7a27741f4210f0b23d7
Jacob Rask <jacob@ernst.site>
2010-05-28 13:28:47 +0200

    Moving articles into a list, building the regexp in a loop, allowing for possible customization of articles later

--- titlesort.py
+++ titlesort.py
@@ -1,6 +1,6 @@
 PLUGIN_NAME = 'Title sort names'
 PLUGIN_AUTHOR = 'Jacob Rask'
-PLUGIN_DESCRIPTION = 'Guesses title album sortnames and adds as titlesort and albumsort tags.'
+PLUGIN_DESCRIPTION = 'Guesses title and album sortnames (language specific) and adds as titlesort and albumsort tags.'
 PLUGIN_VERSION = "0.1.3"
 PLUGIN_API_VERSIONS = ["0.12", "0.12"]
 
@@ -8,13 +8,23 @@ from picard.metadata import register_track_metadata_processor
 from picard.metadata import register_album_metadata_processor
 import re
 
+# define articles
+articles = {}
+articles['eng'] = ['The', 'Tha', 'Da']
+articles['esp'] = ['El', 'La', 'Los', 'Las']
+articles['deu'] = ['Der', 'Das', 'Die']
+articles['fra'] = ['Les', 'Le', 'La']
+articles['swe'] = ['De', 'Den', 'Dom']
+
+# compile regular expressions
 re_articles = {}
-re_articles["eng"] = re.compile(r"The |Tha |Da ")
-re_articles["esp"] = re.compile(r"El |La |Los |Las ")
-re_articles["deu"] = re.compile(r"Der |Das |Die ")
-re_articles["fra"] = re.compile(r"Les |Le |La |L'")
-re_articles["swe"] = re.compile(r"De |Den |Dom ")
-re_articles["mul"] = re.compile(r"The |Tha |Da |El |La |Los |Las |L'|Der |Das |Die |Le |Les |De |Den |Dom ")
+for lang in articles:
+    for n in range(len(articles[lang])):
+        if n == 0:
+            reg = articles[lang][n]
+        else:
+            reg = reg + ' |' + articles[lang][n]
+        re_articles[lang] = re.compile(reg)
 
 def add_titlesort(tagger, metadata, release, track):
     if metadata["titlesort"]:
@@ -29,7 +39,7 @@ def add_titlesort(tagger, metadata, release, track):
     if match:
         sort_prefix = match.group().strip()
         titlesort = sort_re.sub("", titlesort).strip() + ", " + sort_prefix
-        titlesort = titlesort[0].upper() + titlesort[1:] # make sure first letter is capitalized
+        titlesort = titlesort[0].upper() + titlesort[1:] # capitalize first letter
     metadata["titlesort"] = titlesort
 
 def add_albumsort(tagger, metadata, release):
@@ -45,7 +55,7 @@ def add_albumsort(tagger, metadata, release):
     if match:
         sort_prefix = match.group().strip()
         titlesort = sort_re.sub("", titlesort).strip() + ", " + sort_prefix
-        titlesort = titlesort[0].upper() + titlesort[1:] # make sure first letter is capitalized
+        titlesort = titlesort[0].upper() + titlesort[1:] # capitalize first letter
     metadata["albumsort"] = titlesort
 
 register_track_metadata_processor(add_titlesort)
