b8d5afb85d575a55774965a156828a6c7a14b311
Jacob Rask <jacob@ernst.site>
2010-06-01 12:58:10 +0200

    Updated regexp and made global vars local

--- titlesort.py
+++ titlesort.py
@@ -9,30 +9,30 @@ from picard.metadata import register_album_metadata_processor
 import re
 
 # define articles
-articles = {}
-articles['deu'] = ['Der ', 'Das ', 'Die ', 'Eine? '] # German
-articles['eng'] = ['The ', 'Tha ', 'Da ', 'An? '] # English
-articles['esp'] = ['El ', 'La ', 'Los ', 'Las ', 'Una? ', 'Unas ', 'Unos '] # Spanish
-articles['fra'] = ['Les? ', 'La ', 'L\'', 'Une? ', 'Des '] # French
-articles['ita'] = ['Il ', 'Lo ', 'La ', 'Le ', 'L\'', 'I ', 'Gli ', 'Uno? ', 'Una ', 'Un\''] # Italian
-articles['swe'] = ['Den? ', 'Det ', 'Dom ', 'En ', 'Ett '] # Swedish
+_articles = {}
+_articles['deu'] = ['Der ', 'Das ', 'Die ', 'Eine? '] # German
+_articles['eng'] = ['Th[ae] ', 'Da ', 'An? '] # English
+_articles['esp'] = ['El ', 'La ', 'L[ao]s ', 'Una? ', 'Un[ao]s '] # Spanish
+_articles['fra'] = ["Les? ", "La ", "L'", "Une? ", "Des "] # French
+_articles['ita'] = ["Il ", "L[aeo] ", "L'", "I ", "Gli ", "Un[ao]? ", "Un'"] # Italian
+_articles['swe'] = ['De[nt]? ', 'Dom ', 'E(n|tt) '] # Swedish
 
 # compile sort language regular expressions
-re_articles = {}
-regmul = ''
-for lang, a in articles.iteritems():
+_re_articles = {}
+_regmul = ''
+for lang, a in _articles.iteritems():
     reg = ''
     for i in range(len(a)):
-        reg = '|^' + articles[lang][i] + reg
-        re_articles[lang] = re.compile(reg[1:])
-    regmul = regmul + reg
+        reg = '|^' + _articles[lang][i] + reg
+        _re_articles[lang] = re.compile(reg[1:])
+    _regmul = _regmul + reg
 # all articles are collected and used for "multiple languages"
-re_articles['mul'] = re.compile(regmul[1:])
+_re_articles['mul'] = re.compile(_regmul[1:])
 
 def make_sorttitle(title, lang):
-    if lang not in re_articles:
+    if lang not in _re_articles:
         lang = "mul"
-    sort_re = re_articles[lang]
+    sort_re = _re_articles[lang]
     match = sort_re.match(title)
     titlesort = title
     if match:
